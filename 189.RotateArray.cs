9b22dffe8ae0eea8f93c027b4a12553716731edc
Elecky <dalejones@live.cn>
2016-07-01 23:13:09 +0800

    New Solution to Rotate rray

--- 189.RotateArray.cs
+++ 189.RotateArray.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +10,14 @@ namespace RotateArray
     {
         static void Main(string[] args)
         {
+            int[] arr = { 1, 2, 3 };
+            new Solution().Rotate(arr, 2);
+            foreach (int num in arr)
+                Console.WriteLine(num);
         }
     }
 
+    // solution 1, rotate by copying.
     public class Solution
     {
         /// <summary>
@@ -20,6 +25,8 @@ namespace RotateArray
         /// </summary>
         public void Rotate(int[] nums, int k)
         {
+            if (nums.Length == 0)
+                return;
             // simply copy elements to proper position in another array.
             int[] rotatedNums = new int[nums.Length];
             for (int index = 0; index != nums.Length; ++index)
@@ -32,6 +39,52 @@ namespace RotateArray
             {
                 nums[index] = rotatedNums[index];
             }
+        }
+    }
+    
+    // solution 2, rotate in place.
+    public class AnotherSolution
+    {
+        /// <summary>
+        /// Yet another solution to this question.
+        /// Rotate an array of n elements to the right by k steps in place.
+        /// </summary>
+        public void Rotate(int[] nums, int k)
+        {
+            if (nums.Length == 0)
+                return;
+
+            k = k % nums.Length;
+
+            int limit = gcd(nums.Length, k);
+            for (int i = 0; i != limit; ++i)
+            {
+                int j = (i + k) % nums.Length;
+                int temp = nums[j];
+                nums[j] = nums[i];
+
+                while (j != i)
+                {
+                    int t = (j + k) % nums.Length;
+
+                    int temp2 = nums[t];
+                    nums[t] = temp;
+                    temp = temp2;
+
+                    j = t;
+                }
+            }
+        }
+
+        private int gcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int t = a;
+                a = b;
+                b = t % b;
+            }
+            return a;
         }
     }
 }
